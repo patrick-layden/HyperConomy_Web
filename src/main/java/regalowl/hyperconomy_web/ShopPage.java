1aa3cfc1672bc571604f36649fb3734ff158db36
RegalOwl <regalowl@gmail.com>
2015-02-22 17:56:55 -0600

    Don't load shop page if HyperConomy isn't enabled.

--- src/main/java/regalowl/hyperconomy_web/ShopPage.java
+++ src/main/java/regalowl/hyperconomy_web/ShopPage.java
@@ -55,6 +55,7 @@ public class ShopPage extends HttpServlet {
 	private String buildPage(String economy) {
 		try {
 			String page = "";
+			if (!hc.enabled()) return page;
 			if (s == null) {
 				return "";
 			}
